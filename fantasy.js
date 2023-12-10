1d59d9506dbda1d7ea8785e2717a5037655ab690
Theo <106856604+bluesauce3@users.noreply.github.com>
2023-12-10 17:48:44 +1300

    added base pages and files

---
+++ fantasy.js
@@ -0,0 +1,4 @@
+// Add fantasy league functionality here
+// This could include adding players, changing scoring values, etc.
+// You may need to integrate with a backend for data persistence.
+// For simplicity, this example assumes basic functionality.
---
+++ signin.js
@@ -0,0 +1,6 @@
+// Add Microsoft Sign-in logic here
+function signInWithMicrosoft() {
+    // Implement Microsoft Sign-in using Microsoft Graph
+    // Redirect to fantasy.html upon successful sign-in
+    window.location.href = "fantasy.html";
+}
